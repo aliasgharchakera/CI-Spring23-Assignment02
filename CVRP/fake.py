75a4ecd18db2d5fb23ac5a3856004058b8b8cb55
Ali Asghar <aliasghar.chakera@gmail.com>
2023-03-15 13:39:59 +0500

    made simulateAnt multipurpose

--- CVRP/fake.py
+++ CVRP/fake.py
@@ -86,7 +86,7 @@ class AntColonyOptimization:
         # Making Artificial Ants by calculating Routes
         self.ants = []
         for i in range(self.numAnts):
-            ant = self.createAnt()
+            ant = self.simulateAnt(True)
             self.ants.append(ant)
        
     
@@ -121,57 +121,89 @@ class AntColonyOptimization:
 
 
 
-    def simulateAnt(self):
+    def simulateAnt(self, initialize=False):
         # Copying the code from AntMaking as now I have to decide the city with the help of probabilities
         
-        fullRoute = []
+        route = list()
         unvisited = [i for i in range(self.n)]
+        lim = 1
+        if initialize:
+            unvisited = unvisited[1:]
+            lim = 0
         currentCity = self.depot
         truckCapacity = self.capacity
         totalDistance = 0
-        tempRoute = []
-        tempRoute.append(self.depot)
+        path = list()
+        path.append(self.depot)
 
-        while len(unvisited) >1:
+        while len(unvisited) > lim:
             # Choosing random city from unvisited cities
-            potentialCities = []
-            for i in unvisited:
-                if self.demand[i] <= truckCapacity and i!= currentCity:
-                    potentialCities.append(i)
-            
-            probRange = self.calculateProbabilities(currentCity, potentialCities)
+            if initialize:
+                # Choosing random city from unvisited cities
+                i = random.randint(0, len(unvisited) - 1)
+                nextCity = unvisited[i]
+                if self.demand[nextCity] <= truckCapacity:
+                    truckCapacity -= self.demand[nextCity]
+                    totalDistance += self.distances[currentCity][nextCity]
+                    path.append(nextCity)
+                    currentCity = nextCity
+                    # Now its time to pop the city from unvisited
+                    unvisited.pop(i)
+                else:
+                    # This means that the truck is full and we need to go back to depot and line 70 will simply add the distance of depot to current city or vice versa
 
-            # Choosing the Random number
-            randNum = random.random()
-            for i in range(len(probRange)):
-                if randNum >= probRange[i][0] and randNum <= probRange[i][1]:
-                    selectedCity = i
-                    break
-            
-            nextCity = potentialCities[selectedCity]
-            truckCapacity -= self.demand[nextCity]
-            totalDistance += self.distances[currentCity][nextCity]
-
-            currentCity = nextCity
-            tempRoute.append(currentCity)
-
-            if currentCity == self.depot:
-                truckCapacity = self.capacity
-                fullRoute.append(tempRoute)
-                tempRoute = [self.depot]
+                    totalDistance += self.distances[currentCity][self.depot]
+                    route.append(path)
+                    # Changing the capacity of vehicles
+                    truckCapacity = self.capacity
+                    path = list()
+                    path.append(self.depot)
+                    currentCity = self.depot
+                    truckCapacity -= self.demand[nextCity]
+                    totalDistance += self.distances[currentCity][nextCity]
+                    path.append(nextCity)
+                    currentCity = nextCity
+                    unvisited.pop(i)
             else:
+                # Choosing the city with the help of probabilities
+                potentialCities = list()
                 for i in unvisited:
-                    if i == nextCity:
-                        unvisited.remove(i)
+                    if self.demand[i] <= truckCapacity and i!= currentCity:
+                        potentialCities.append(i)
+                
+                proportionalProbabilities = self.calculateProbabilities(currentCity, potentialCities)
+
+                # Choosing the Random number
+                p = random.random()
+                for i in range(len(proportionalProbabilities)):
+                    if p >= proportionalProbabilities[i][0] and p < proportionalProbabilities[i][1]:
+                        selectedCity = i
                         break
+                
+                nextCity = potentialCities[selectedCity]
+                truckCapacity -= self.demand[nextCity]
+                totalDistance += self.distances[currentCity][nextCity]
+
+                currentCity = nextCity
+                path.append(currentCity)
+
+                if currentCity == self.depot:
+                    truckCapacity = self.capacity
+                    route.append(path)
+                    path = [self.depot]
+                else:
+                    for i in unvisited:
+                        if i == nextCity:
+                            unvisited.remove(i)
+                            break
 
         # Now the path will again go to Depot to make a full route
-        tempRoute.append(self.depot)
+        path.append(self.depot)
         totalDistance += self.distances[currentCity][self.depot]
 
-        fullRoute.append(tempRoute)
+        route.append(path)
 
-        return Ant(fullRoute, totalDistance)
+        return Ant(route, totalDistance)
 
 
 
